6346364cc06c2d9d3c428f00d7c856a4f61469b4
ShafiqueNaveed <shafiquenaveed00001@gmail.com>
2024-03-21 20:17:14 +0500

    setup and requirement

--- template.py
+++ template.py
@@ -30,7 +30,6 @@ for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-
     if filedir !="":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
@@ -39,7 +38,6 @@ for filepath in list_of_files:
         with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
-
 
     else:
         logging.info(f"{filename} is already exists")
