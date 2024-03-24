ab178f59f02323ce49f0473c7b6166c9a934b8f6
ShafiqueNaveed <shafiquenaveed00001@gmail.com>
2024-03-24 14:41:48 +0500

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from chest_cancer_classification import logger
+from chest_cancer_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to CNN Classification")
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
